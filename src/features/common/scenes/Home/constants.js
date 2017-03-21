4abd38b4a4db53ee30a61881c8daad022cc8f7ab
goncy <goncy@live.com.ar>
2017-03-21 14:40:50 -0300

    Send images to constants files

---
+++ src/features/common/scenes/Home/constants.js
@@ -0,0 +1,16 @@
+export const SLIDER_IMAGES = [
+  {
+    original: 'https://scontent-gru2-1.xx.fbcdn.net/v/t31.0-8/16402775_1352422904816950_6765133457965009813_o.jpg?oh=425febedb32ff2451fd8592b01a29348&oe=595AD6D3',
+    thumbnail: 'https://scontent-gru2-1.xx.fbcdn.net/v/t31.0-8/16402775_1352422904816950_6765133457965009813_o.jpg?oh=425febedb32ff2451fd8592b01a29348&oe=595AD6D3'
+  },
+  {
+    original: 'https://fb-s-d-a.akamaihd.net/h-ak-xla1/v/t1.0-9/12347947_1028633040529273_5416443376694321653_n.jpg?oh=94cdf8957b88d5f540abfca14bb68b56&oe=596CD3CB&__gda__=1498904594_a0871115e4cca6f35c922ec358b3a23c',
+    thumbnail: 'https://fb-s-d-a.akamaihd.net/h-ak-xla1/v/t1.0-9/12347947_1028633040529273_5416443376694321653_n.jpg?oh=94cdf8957b88d5f540abfca14bb68b56&oe=596CD3CB&__gda__=1498904594_a0871115e4cca6f35c922ec358b3a23c'
+  },
+  {
+    original: 'https://scontent-gru2-1.xx.fbcdn.net/v/t1.0-9/12313700_1022978377761406_3000394968014256743_n.jpg?oh=789dfe441ddad598b87ff5a2962391f8&oe=596E23AE',
+    thumbnail: 'https://scontent-gru2-1.xx.fbcdn.net/v/t1.0-9/12313700_1022978377761406_3000394968014256743_n.jpg?oh=789dfe441ddad598b87ff5a2962391f8&oe=596E23AE'
+  }
+]
+
+export const FACEBOOK_IFRAME_URL = 'https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FClubBernal&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId'
--- src/features/common/scenes/Home/index.js
+++ src/features/common/scenes/Home/index.js
@@ -4,20 +4,7 @@ import ImageGallery from 'react-image-gallery'
 
 import './Home.css'
 
-const images = [
-  {
-    original: 'https://scontent-gru2-1.xx.fbcdn.net/v/t31.0-8/16402775_1352422904816950_6765133457965009813_o.jpg?oh=425febedb32ff2451fd8592b01a29348&oe=595AD6D3',
-    thumbnail: 'https://scontent-gru2-1.xx.fbcdn.net/v/t31.0-8/16402775_1352422904816950_6765133457965009813_o.jpg?oh=425febedb32ff2451fd8592b01a29348&oe=595AD6D3'
-  },
-  {
-    original: 'https://fb-s-d-a.akamaihd.net/h-ak-xla1/v/t1.0-9/12347947_1028633040529273_5416443376694321653_n.jpg?oh=94cdf8957b88d5f540abfca14bb68b56&oe=596CD3CB&__gda__=1498904594_a0871115e4cca6f35c922ec358b3a23c',
-    thumbnail: 'https://fb-s-d-a.akamaihd.net/h-ak-xla1/v/t1.0-9/12347947_1028633040529273_5416443376694321653_n.jpg?oh=94cdf8957b88d5f540abfca14bb68b56&oe=596CD3CB&__gda__=1498904594_a0871115e4cca6f35c922ec358b3a23c'
-  },
-  {
-    original: 'https://scontent-gru2-1.xx.fbcdn.net/v/t1.0-9/12313700_1022978377761406_3000394968014256743_n.jpg?oh=789dfe441ddad598b87ff5a2962391f8&oe=596E23AE',
-    thumbnail: 'https://scontent-gru2-1.xx.fbcdn.net/v/t1.0-9/12313700_1022978377761406_3000394968014256743_n.jpg?oh=789dfe441ddad598b87ff5a2962391f8&oe=596E23AE'
-  }
-]
+import {SLIDER_IMAGES, FACEBOOK_IFRAME_URL} from './constants'
 
 const Home = () => (
   <div className='Home animated fadeIn'>
@@ -30,7 +17,7 @@ const Home = () => (
           <div className="row">
             <ImageGallery
               showPlayButton={false}
-              items={images}
+              items={SLIDER_IMAGES}
               slideInterval={2000}
             />
           </div>
@@ -51,7 +38,7 @@ const Home = () => (
       <div className="row has-text-centered">
         <div className="notification is-primary">
           <iframe
-            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FClubBernal&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+            src={FACEBOOK_IFRAME_URL}
             width="340"
             height="500"
             style={{border: 'none', overflow: 'hidden'}}
